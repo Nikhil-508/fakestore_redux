218bf50376b581438c866dc1632d347505761bb3
Nikhil <nikhilbchandran@gmail.com>
2025-03-23 15:12:49 +0530

    favcon and title added

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <div className="container mx-auto p-4">
-          <h1 className="text-2xl font-bold text-center">FakeStore App</h1>
+          <h1 className="text-2xl font-bold text-center text-red">FakeStore App</h1>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/product/:id" element={<ProductDetailPage />} />
